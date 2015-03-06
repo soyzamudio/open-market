0127dc38957481e1ee215a415e2f22d681b4afa9
Jose Zamudio <zamudio@outlook.com>
2015-03-06 13:41:46 -0800

    Swap completed. Email Done. Time for styling. Fuck Yeah.

--- server/routes/items/makeSwap.js
+++ server/routes/items/makeSwap.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var Item = require('../../models/item');
-// var mandrill = require('node-mandrill')(process.env.MANDRILL_KEY);
-
+var mandrill = require('node-mandrill')(process.env.MANDRILL_KEY);
+//
 module.exports = {
   handler: function(request, reply) {
-    Item.findOne({_id: request.payload.params.itemId}).exec(function(err, item) {
-      Item.findOne({_id: request.payload.params.swapId}).exec(function(errTwo, itemTwo) {
+    Item.findOne({_id: request.payload.params.itemId}).populate('userId').exec(function(err, item) {
+      Item.findOne({_id: request.payload.params.swapId}).populate('userId').exec(function(errTwo, itemTwo) {
         var itemOneUser = item.userId;
         var itemTwoUser = itemTwo.userId;
 
@@ -25,27 +25,28 @@ module.exports = {
         item.timesSwapped++;
         itemTwo.timesSwapped++;
 
+        console.log(item.name);
+
         item.save(function(err){
           itemTwo.save(function(err){
             if (err) { reply().code(400); }
-            else { reply().code(200); }
-          })
-        })
+            else {
+              mandrill('/messages/send', {
+                message: {
+                  to: [{email: item.userId.email, name: item.userId.name}],
+                  from_email: itemTwo.userId.email,
+                  subject: 'Swaped Completed!',
+                  text: itemTwo.userId.name + ' accepted your swap! Now ' + item.name + ' belongs to you!'
+                }
+              }, function(error, response) {
+                if (error) { console.log(JSON.stringify(error)); }
+                else { console.log(response); }
+              });
+              reply().code(200);
+            }
+          });
+        });
       });
     });
   }
 };
-
-
-
-
-// mandrill('/messages/send', { message: {
-//   to: [{email: item.userId.email, name: item.userId.name}],
-//   from_email: itemTwo.userId.email,
-//   subject: 'Someone want to swap for your ' + item.name + '!',
-//   text: 'Jose wants to swap his ' + itemTwo.name + ' for your ' + item.name + '!\n http://localhost:3333/#/items/' + itemTwo._id
-//   }
-// }, function(error, response) {
-//   if (error) { console.log(JSON.stringify(error)); }
-//   else { console.log(response); }
-// });
