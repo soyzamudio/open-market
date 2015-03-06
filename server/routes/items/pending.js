51b0bd30ab959e6bbb52c245725b44ac3fa67010
Jose Zamudio <zamudio@outlook.com>
2015-03-05 23:17:46 -0800

    Send email when requesting swap

--- server/routes/items/pending.js
+++ server/routes/items/pending.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Item = require('../../models/item');
+var mandrill = require('node-mandrill')(process.env.MANDRILL_KEY);
 
 module.exports = {
   handler: function(request, reply) {
@@ -10,7 +11,19 @@ module.exports = {
         item.status = 'pending';
         item.save(function(err) {
           if (err || errTwo) { reply().code(400); }
-          else { reply().code(200); }
+          else {
+            mandrill('/messages/send', { message: {
+              to: [{email: item.userId.email, name: item.userId.name}],
+              from_email: itemTwo.userId.email,
+              subject: 'Someone want to swap for your ' + item.name + '!',
+              text: 'Jose wants to swap his ' + itemTwo.name + ' for your ' + item.name + '!'
+              }
+            }, function(error, response) {
+              if (error) { console.log(JSON.stringify(error)); }
+              else { console.log(response); }
+            });
+            reply().code(200);
+          }
         });
       });
     });
