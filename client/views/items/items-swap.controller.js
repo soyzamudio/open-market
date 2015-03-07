2ce2f87ebd3b23bce6548d9194f146db6f69f9a7
emilemuny <emilemuny007@gmail.com>
2015-03-07 14:16:16 -0800

    adding Swap page UI changes

--- client/views/items/items-swap.controller.js
+++ client/views/items/items-swap.controller.js
@@ -4,6 +4,7 @@ angular.module('open-market')
   .controller('SwapCtrl', ['$scope', '$state', 'Item', function($scope, $state, Item) {
     Item.getPending().then(function(response) {
       $scope.pending = response.data.items;
+      console.log($scope.pending);
     });
 
     $scope.acceptSwap = function(itemId, swapId) {
--- server/routes/items/swap.js
+++ server/routes/items/swap.js
@@ -1,12 +1,15 @@
 'use strict';
 
 var Item = require('../../models/item');
+var User = require('../../models/user');
 
 module.exports = {
   handler: function(request, reply) {
     Item.find({userId: request.auth.credentials._id, status: 'pending'}).populate('pending').exec(function(err, items) {
-      if (err) { reply().code(400); }
-      else { reply({items:items}).code(200); }
+      Item.populate(items, {path: 'pending.userId', model: User}, function() {
+        if (err) { reply().code(400); }
+        else { reply({items:items}).code(200); }
+      });
     });
   }
 };
