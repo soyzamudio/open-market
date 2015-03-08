ec6cc3f7b20451673b6306534d132baa490380dd
emilemuny <emilemuny007@gmail.com>
2015-03-08 16:44:05 -0700

    added return user items to the Ctrl/View user-show page

--- client/views/users/user-show.controller.js
+++ client/views/users/user-show.controller.js
@@ -4,5 +4,8 @@ angular.module('open-market')
    .controller('UserShowCtrl', ['$rootScope', '$scope', '$state', 'User', function($rootScope, $scope, $state, User) {
      User.show($state.params.userId).then(function(response) {
        $scope.user = response.data.user;
+       $scope.items = response.data.items;
+       console.log(response.data.user);
+       console.log('#####Item',response.data.items);
      });
 }]);
--- server/routes/users/show.js
+++ server/routes/users/show.js
@@ -1,14 +1,19 @@
 'use strict';
 
 var User = require('../../models/user');
+var Item = require('../../models/item');
 
 module.exports = {
   auth: false,
   handler: function(request, reply) {
     User.findOne({_id:request.params.userId}, function(err, user) {
-      console.log('****User***');
-      if(err) { reply().code(400); }
-      reply({user:user}).code(200);
+      Item.find({userId: user._id}, function(err, items) {
+        console.log('****User***');
+        if(err) { reply().code(400); }
+        console.log('$$$$$$$', items)
+        reply({user:user, items:items}).code(200);
+
+      });
     });
   }
 };
