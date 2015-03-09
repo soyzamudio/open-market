68dd860864506069a49028239d1fcbe447415a84
Jose Zamudio <zamudio@outlook.com>
2015-03-09 11:16:15 -0700

    Fix bug

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('open-market', ['ui.router', 'file-model'])
+angular.module('open-market', ['ui.router'])
   .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
     $stateProvider
